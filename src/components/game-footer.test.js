4e19c7ca0fd6311882d092a8d6a3a9dc14c1f896
iastanley <iasvks.craigslist@gmail.com>
2017-05-02 12:00:42 -0700

    finished unit tests for game-footer component

--- src/components/game-footer.test.js
+++ src/components/game-footer.test.js
@@ -7,4 +7,11 @@ describe('<GameFooter />', () => {
   it('Should shallow render', () => {
     shallow(<GameFooter />);
   });
+
+  it('Should render div with class and guesses', () => {
+    const guesses = [25, 50, 75];
+    const wrapper = shallow(<GameFooter guesses={guesses}/>);
+    expect(wrapper.hasClass('game-footer')).toBe(true);
+    expect(wrapper.find('.guess')).toHaveLength(guesses.length);
+  });
 });
