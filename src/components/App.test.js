78c8faa56268c8849b8864d0908e2fa391fd7ff3
iastanley <iasvks.craigslist@gmail.com>
2017-05-02 16:36:57 -0700

    finished unit tests for GameBody and App comcomponents

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,23 +4,45 @@ import {shallow, mount} from 'enzyme';
 import App from './App';
 
 describe('<App />', () => {
-  let guessList = [];
-  beforeAll(()=> {
-    for(let i = 1; i < 4; i++) {
-      guessList.push(i);
-    }
-  });
 
+  //test if app component crashes on render
   it('It should shallow render', ()=>{
     shallow(<App />);
   });
 
-  it('Should render game footer', () => {
-    const element = shallow(<App/>);
-    // let setCallback = (guess) => element.instance().setGuess(guess);
-    let setCallback = element.instance().setGuess;
-    guessList.forEach(setCallback);
-    expect(element.find('GameFooter').prop('guesses').length).toEqual(3);
+  //test that all child components are rendered
+  it('Should render child components', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('GameLink')).toHaveLength(2);
+    expect(wrapper.find('GameHeader')).toHaveLength(1);
+    expect(wrapper.find('GameBody')).toHaveLength(1);
+    expect(wrapper.find('GameFooter')).toHaveLength(1);
+  });
+
+  //test toggleInstructions
+  it('Should show instructions on link click', () => {
+    const wrapper = mount(<App/>);
+    wrapper.find('a .left').simulate('click');
+    expect(wrapper.find('Instructions')).toHaveLength(1);
+  });
+
+  //test resetGame on click
+  it('Should call resetGame when New Game link clicked', () => {
+    const wrapper = mount(<App/>);
+    const instance = wrapper.instance();
+    wrapper.find('a .right').simulate('click');
+    //testing that state object has been reset
+    expect(wrapper.state('prevGuesses')).toHaveLength(0);
+  });
+
+  //should render footer with correct props from state object
+  it('Should render GameFooter & GameBody with correct props', () => {
+    let guessList = [25, 50, 75];
+    const wrapper = shallow(<App/>);
+    guessList.forEach(wrapper.instance().setGuess);
+    //both props are based on state.prevGuesses value
+    expect(wrapper.find('GameFooter').prop('guesses')).toHaveLength(guessList.length);
+    expect(wrapper.find('GameBody').prop('guessCount')).toBe(guessList.length);
   });
 
   it('Should give error feedback for invalid input', ()=> {
@@ -34,15 +56,21 @@ describe('<App />', () => {
     expect(element.find('GameHeader').prop('feedback')).toEqual('Not a valid input!');
   });
 
-  it('Should show instructions', () => {
-    const element = mount(<App />);
-    element.find('.left a').simulate('click');
-    expect(element.find('Instructions').length).toEqual(1);
+  it('Should give correct feedback for valid input', () => {
+    const wrapper = shallow(<App/>);
+    wrapper.setState({
+      correctNumber: 1
+    });
+    wrapper.instance().setGuess(50);
+    expect(wrapper.find('GameHeader').prop('feedback')).toBe('cold');
+    wrapper.instance().setGuess(8);
+    expect(wrapper.find('GameHeader').prop('feedback')).toBe('Warm');
+    wrapper.instance().setGuess(2);
+    expect(wrapper.find('GameHeader').prop('feedback')).toBe('Hot!');
+    //this must go last due to behavior after correct guess
+    wrapper.instance().setGuess(1);
+    expect(wrapper.find('GameHeader').prop('feedback')).toBe('Correct!');
+
+
   });
-
-  //check children render
-
-  //check props of children
-
-  //
 });
--- src/components/game-body.test.js
+++ src/components/game-body.test.js
@@ -7,4 +7,43 @@ describe('<GameBody />', ()=> {
   it('Should shallow render', () => {
     shallow(<GameBody />);
   });
+
+  it('Should render the form, input, button and p', () => {
+    const wrapper = shallow(<GameBody />);
+    expect(wrapper.hasClass('game-body')).toBe(true);
+    expect(wrapper.find('form')).toHaveLength(1);
+    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find('button')).toHaveLength(1);
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+
+  it('Should render props when set', () => {
+    const guessCount = 5;
+    const wrapper = shallow(<GameBody guessCount={guessCount}/>);
+    expect(wrapper.find('p').contains(
+      <span className="guess-count">
+        {guessCount}
+      </span>
+    )).toEqual(true);
+  });
+
+  it('Should update input value on user input', () => {
+    const simEvent = {
+      target: {
+        value: '50'
+      }
+    };
+    const wrapper = shallow(<GameBody />);
+    const instance = wrapper.instance();
+    instance.handleChange(simEvent);
+    expect(wrapper.state('input')).toEqual('50');
+  });
+
+  it('Should call callback function passed as prop', () => {
+    const callback = jest.fn();
+    //full DOM rendering is required to test submit and callback
+    const wrapper = mount(<GameBody onGuess={callback}/>);
+    wrapper.find('form').simulate('submit');
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
 });
